951f52029666a3689064474c321e878c29492fa9
Khushi-tw <khushit355@gmail.com>
2025-03-09 11:51:57 +0000

    First Flask page

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 # create a Route
 @app.route("/")
 def hello_world():
-    return "Hello World!"
+    return "Hello Khushi!"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=True)
