eef23e9a75d12fcb06908e0259efc462fee0e5a8
Khushi-tw <khushit355@gmail.com>
2025-04-02 17:35:35 +0000

    recent commit

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
-from flask import Flask
+from flask import Flask,render_template
 # creating an object of Flask
 app = Flask(__name__)
 # create a Route
 @app.route("/")
 def hello_world():
-    return "Hello Khushi!"
+    return render_template("home.html")
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=True)
